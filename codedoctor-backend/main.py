c4f14f219661adb5dc8aad170cecd0efb58d300c
Jethro Ibebuike <jethromitchell4@gmail.com>
2026-09-06 16:54:51 +0100

    Allow production frontend in CORS

--- codedoctor-backend/main.py
+++ codedoctor-backend/main.py
@@ -19,7 +19,8 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:5173",
     "http://localhost:5174",
-    ],
+    "https://codedoctor-uejj.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
